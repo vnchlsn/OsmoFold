838ea7f7dfa77efcdefb7d2c12ad8179650d4f4e
Vincent Nicholson <35417297+vnchlsn@users.noreply.github.com>
2025-07-10 22:02:04 -0500

    Whoops

--- build/lib/osmofold/__init__.py
+++
@@ -1,4 +0,0 @@
-# osmofold/__init__.py
-from .osmofold_local import get_tfe, sasa_to_rasa, protein_unfolded_dG, protein_folded_dG, protein_ddG_folding
-from .parallel import batch_process_pdbs
-from .osmofold_lite import read_fasta, protein_ddG_folding_lite, protein_folded_dG_lite, protein_unfolded_dG_lite
